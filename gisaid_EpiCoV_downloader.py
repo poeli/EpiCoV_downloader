1bfd87b9cb04ad571a8d14abb3ab612b197bb2d2
Paul Li <paulli@pn1704858.lanl.gov>
2020-04-27 22:50:32 -0600

    update for downloading seqeucnes and metadata from partner downloads

--- gisaid_EpiCoV_downloader.py
+++ gisaid_EpiCoV_downloader.py
@@ -84,20 +84,22 @@ def download_gisaid_EpiCoV(uname, upass, headless, wd, cs, ce, ss, se, cg, hc, t
         os.makedirs(wd, exist_ok=True)
 
     wd = os.path.abspath(wd)
-    GISAID_FASTA = f'{wd}/gisaid_cov2020_sequences.fasta'
+    GISAID_FASTA = f'{wd}/sequences.fasta.bz2'
     GISAID_TABLE = f'{wd}/gisaid_cov2020_acknowledgement_table.xls'
     GISAID_JASON = f'{wd}/gisaid_cov2020_metadata.json'
+    GISAID_TSV   = f'{wd}/metadata.tsv.bz2'
     metadata = []
 
     # MIME types
     mime_types = "application/octet-stream"
     mime_types += ",application/excel,application/vnd.ms-excel"
     mime_types += ",application/pdf,application/x-pdf"
+    mime_types += ",application/x-bzip2"
 
     # start fresh
     try:
         os.remove(GISAID_FASTA)
-        os.remove(GISAID_TABLE)
+        os.remove(GISAID_TSV)
     except OSError:
         pass
 
@@ -146,208 +148,259 @@ def download_gisaid_EpiCoV(uname, upass, headless, wd, cs, ce, ss, se, cg, hc, t
     waiting_sys_timer(wait)
 
     # Downloading preliminary analysis summary PDF
-    print("Downloading preliminary analysis summary...")
-    dl_button = wait.until(EC.element_to_be_clickable(
-        (By.XPATH, '//a[contains(text(), "download")]')))
+    print("Downloading analysis update pdf...")
+    dl_button = driver.find_element_by_xpath('//div[contains(text(), "analysis update.pdf")]')
+    driver.execute_script("arguments[0].scrollIntoView();", dl_button)
     dl_button.click()
     waiting_sys_timer(wait)
 
-    print("Browsing EpiCoV...")
-    browse_tab = wait.until(EC.element_to_be_clickable(
-        (By.XPATH, '//*[contains(text(), "Browse")]')))
-    browse_tab.click()
-    waiting_sys_timer(wait)
-    waiting_table_to_get_ready(wait)
+    # download from partner downloads
+    print("Clicking partner downloads...")
+    pd_button = driver.find_element_by_xpath('//div[contains(text(), "partner downloads")]')
 
-    # set dates
-    date_inputs = driver.find_elements_by_css_selector(
-        "div.sys-form-fi-date input")
-    dates = (cs, ce, ss, se)
-    for dinput, date in zip(date_inputs, dates):
-        if date:
-            dinput.send_keys(date)
-
-    ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-    waiting_sys_timer(wait, 7)
-
-    # complete genome only
-    if cg:
-        button = driver.find_element_by_xpath('//input[@value="complete"]')
-        button.click()
-        waiting_sys_timer(wait)
-
-    # high coverage only
-    if hc:
-        button = driver.find_element_by_xpath('//input[@value="highq"]')
-        button.click()
-        waiting_sys_timer(wait)
-
-    # download fasta
-    retry = 0
+    # have to click the first row twice to start the iframe
+    iframe = None
+    retry = 1
     while retry <= rt:
         try:
-            print("Selecting all sequences...")
-            # select all
-            button_sa = driver.find_element_by_css_selector("span.yui-dt-label input")
-            button_sa.click()
-            waiting_sys_timer(wait)
- 
-            print("Downloading the sequence file...")
-            button = driver.find_element_by_xpath(
-                "//td[@class='sys-datatable-info']/button")
+            pd_button.click()
+            iframe = driver.find_element_by_xpath("//iframe")
+            if iframe:
+                break
+            else:
+                raise
+        except:
+            print(f"retrying...#{retry} in {iv} sec(s)")
+            if retry == rt:
+                print("Failed")
+                sys.exit(1)
+            else:
+                time.sleep(iv)
+                retry += 1
+    
+    driver.switch_to.frame(iframe)
+    waiting_sys_timer(wait)
+    
+    print("Downloading sequences.fasta.bz2...")
+    dl_button = wait.until(EC.element_to_be_clickable(
+        (By.XPATH, '//div[contains(text(), "sequences.fasta.bz2")]')))
+    dl_button.click()
+    waiting_sys_timer(wait)
+
+    print("Downloading metadata.tsv.bz2...")
+    dl_button = wait.until(EC.element_to_be_clickable(
+        (By.XPATH, '//div[contains(text(), "metadata.tsv.bz2")]')))
+    dl_button.click()
+    waiting_sys_timer(wait)
+
+    back_button = wait.until(EC.element_to_be_clickable(
+        (By.XPATH, '//button[contains(text(), "Back")]')))
+    back_button.click()
+
+    driver.switch_to.default_content()
+
+    if not os.path.isfile(GISAID_FASTA) or not os.path.isfile(GISAID_TSV):
+        time.sleep(30)
+    if not os.path.isfile(GISAID_FASTA) or not os.path.isfile(GISAID_TSV):
+        time.sleep(30)
+
+    if (cs and ce) or (ss and se):
+        print("Browsing EpiCoV...")
+        browse_tab = wait.until(EC.element_to_be_clickable(
+            (By.XPATH, '//*[contains(text(), "Browse")]')))
+        browse_tab.click()
+        waiting_sys_timer(wait)
+        waiting_table_to_get_ready(wait)
+
+        # set dates
+        date_inputs = driver.find_elements_by_css_selector(
+            "div.sys-form-fi-date input")
+        dates = (cs, ce, ss, se)
+        for dinput, date in zip(date_inputs, dates):
+            if date:
+                dinput.send_keys(date)
+
+        ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+        waiting_sys_timer(wait, 7)
+
+        # complete genome only
+        if cg:
+            button = driver.find_element_by_xpath('//input[@value="complete"]')
             button.click()
             waiting_sys_timer(wait)
-            if not download_finished(GISAID_FASTA, 180):
-                raise
-            break
-        except:
-            print(f"retrying...#{retry} in {iv} sec(s)")
-            if retry == rt:
-                print("Failed")
-                sys.exit(1)
-            else:
-                time.sleep(iv)
-                retry += 1
 
-    retry = 0
-    while retry <= rt:        
-        try:
-            print("Downloading the acknowledgement table...")
-            elem = driver.find_element_by_xpath(
-                "/html/body/form/div[5]/div/div[3]/div[1]/div/center[1]/a")
-            script = elem.get_attribute("onclick")
-            driver.execute_script(f"return {script}")
+        # high coverage only
+        if hc:
+            button = driver.find_element_by_xpath('//input[@value="highq"]')
+            button.click()
             waiting_sys_timer(wait)
-            if not download_finished(GISAID_TABLE, 180):
-                raise
-            break
-        except:
-            print(f"retrying...#{retry} in {iv} sec(s)")
-            if retry == rt:
-                print("Failed")
-                sys.exit(1)
-            else:
-                time.sleep(iv)
-                retry += 1
 
-    # iterate each pages
-    if meta_dl:
-        page_num = 1
-        print("Retrieving metadata...")
-        while True:
-            print(f"Starting processing page# {page_num}...")
-            # retrieve tables
-            tbody = wait.until(
-                EC.presence_of_element_located(
-                    (By.XPATH, "//tbody[@class='yui-dt-data']"))
-            )
+        # download fasta
+        retry = 0
+        while retry <= rt:
+            try:
+                print("Selecting all sequences...")
+                # select all
+                button_sa = driver.find_element_by_css_selector("span.yui-dt-label input")
+                button_sa.click()
+                waiting_sys_timer(wait)
+    
+                print("Downloading the sequence file...")
+                button = driver.find_element_by_xpath(
+                    "//td[@class='sys-datatable-info']/button")
+                button.click()
+                waiting_sys_timer(wait)
+                if not download_finished(GISAID_FASTA, 180):
+                    raise
+                break
+            except:
+                print(f"retrying...#{retry} in {iv} sec(s)")
+                if retry == rt:
+                    print("Failed")
+                    sys.exit(1)
+                else:
+                    time.sleep(iv)
+                    retry += 1
 
-            waiting_table_to_get_ready(wait)
+        retry = 0
+        while retry <= rt:        
+            try:
+                print("Downloading the acknowledgement table...")
+                elem = driver.find_element_by_xpath(
+                    "/html/body/form/div[5]/div/div[3]/div[1]/div/center[1]/a")
+                script = elem.get_attribute("onclick")
+                driver.execute_script(f"return {script}")
+                waiting_sys_timer(wait)
+                if not download_finished(GISAID_TABLE, 180):
+                    raise
+                break
+            except:
+                print(f"retrying...#{retry} in {iv} sec(s)")
+                if retry == rt:
+                    print("Failed")
+                    sys.exit(1)
+                else:
+                    time.sleep(iv)
+                    retry += 1
 
-            # interate each row
-            for tr in tbody.find_elements_by_tag_name("tr"):
-                td = tr.find_element_by_tag_name("td")
-                driver.execute_script("arguments[0].scrollIntoView();", td)
-
-                # have to click the first row twice to start the iframe
-                iframe = None
-                record_elem = None
-                retry = 1
-                while retry <= rt:
-                    try:
-                        td.click()
-                        waiting_sys_timer(wait)
-                        iframe = driver.find_element_by_xpath("//iframe")
-                        if iframe:
-                            break
-                        else:
-                            raise
-                    except:
-                        print(f"retrying...#{retry} in {iv} sec(s)")
-                        if retry == rt:
-                            print("Failed")
-                            sys.exit(1)
-                        else:
-                            time.sleep(iv)
-                            retry += 1
-
-                driver.switch_to.frame(iframe)
-
-                # detect error: "An internal server error occurred."
-                # and "error-token: DYX47"
-                error_token = driver.find_element_by_xpath("//b")
-                if error_token:
-                    error_token_text = error_token.text
-                    if "error-token" in error_token.text:
-                        print(
-                            "[FATAL ERROR] A website internal server error occurred.")
-                        print(error_token_text)
-                        sys.exit(1)
-
-                # get the element of table with metadata
-                record_elem = wait.until(
+        # iterate each pages
+        if meta_dl:
+            page_num = 1
+            print("Retrieving metadata...")
+            while True:
+                print(f"Starting processing page# {page_num}...")
+                # retrieve tables
+                tbody = wait.until(
                     EC.presence_of_element_located(
-                        (By.XPATH, "//div[@class='packer']"))
+                        (By.XPATH, "//tbody[@class='yui-dt-data']"))
                 )
 
-                # parse metadata
-                m = getMetadata(record_elem)
-                metadata.append(m)
-                print(f"{m['Accession ID']}\t{m['Virus name']}")
+                waiting_table_to_get_ready(wait)
 
-                # get back
-                ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-                time.sleep(1)
-                driver.switch_to.default_content()
+                # interate each row
+                for tr in tbody.find_elements_by_tag_name("tr"):
+                    td = tr.find_element_by_tag_name("td")
+                    driver.execute_script("arguments[0].scrollIntoView();", td)
 
-            print(f"Compeleted page# {page_num}.")
-            page_num += 1
+                    # have to click the first row twice to start the iframe
+                    iframe = None
+                    record_elem = None
+                    retry = 1
+                    while retry <= rt:
+                        try:
+                            td.click()
+                            waiting_sys_timer(wait)
+                            iframe = driver.find_element_by_xpath("//iframe")
+                            if iframe:
+                                break
+                            else:
+                                raise
+                        except:
+                            print(f"retrying...#{retry} in {iv} sec(s)")
+                            if retry == rt:
+                                print("Failed")
+                                sys.exit(1)
+                            else:
+                                time.sleep(iv)
+                                retry += 1
 
-            # go to the next page
-            retry = 1
-            button_next_page = None
-            try:
-                button_next_page = driver.find_element_by_xpath(
-                    f'//a[@page="{page_num}"]')
-            except:
-                break
+                    driver.switch_to.frame(iframe)
 
-            if button_next_page:
-                print(f"Entering page# {page_num}...")
-                while retry <= rt:
-                    try:
-                        button_next_page.click()
-                        time.sleep(10)
-                        current_page = driver.find_element_by_xpath(
-                            '//span[@class="yui-pg-current-page yui-pg-page"]').text
-                        if current_page != str(page_num):
-                            raise
-                        else:
-                            break
-                    except:
-                        print(f"retrying...#{retry} in {iv} sec(s)")
-                        if retry == rt:
-                            print("Failed")
+                    # detect error: "An internal server error occurred."
+                    # and "error-token: DYX47"
+                    error_token = driver.find_element_by_xpath("//b")
+                    if error_token:
+                        error_token_text = error_token.text
+                        if "error-token" in error_token.text:
+                            print(
+                                "[FATAL ERROR] A website internal server error occurred.")
+                            print(error_token_text)
                             sys.exit(1)
-                        else:
-                            time.sleep(iv)
-                            retry += 1
 
-        # writing metadata to JSON file
-        print("Writing metadata...")
-        with open(GISAID_JASON, 'w') as outfile:
-            json.dump(metadata, outfile)
+                    # get the element of table with metadata
+                    record_elem = wait.until(
+                        EC.presence_of_element_located(
+                            (By.XPATH, "//div[@class='packer']"))
+                    )
 
-    # wait for download to complete
-    if not os.path.isfile(GISAID_FASTA) or not os.path.isfile(GISAID_TABLE):
-        time.sleep(30)
-    if not os.path.isfile(GISAID_FASTA) or not os.path.isfile(GISAID_TABLE):
-        time.sleep(30)
-    if os.path.isfile(GISAID_FASTA) and os.path.isfile(GISAID_TABLE):
-        while (os.stat(GISAID_FASTA).st_size == 0):
-            time.sleep(5)
-        while (os.stat(GISAID_TABLE).st_size == 0):
-            time.sleep(5)
+                    # parse metadata
+                    m = getMetadata(record_elem)
+                    metadata.append(m)
+                    print(f"{m['Accession ID']}\t{m['Virus name']}")
+
+                    # get back
+                    ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+                    time.sleep(1)
+                    driver.switch_to.default_content()
+
+                print(f"Compeleted page# {page_num}.")
+                page_num += 1
+
+                # go to the next page
+                retry = 1
+                button_next_page = None
+                try:
+                    button_next_page = driver.find_element_by_xpath(
+                        f'//a[@page="{page_num}"]')
+                except:
+                    break
+
+                if button_next_page:
+                    print(f"Entering page# {page_num}...")
+                    while retry <= rt:
+                        try:
+                            button_next_page.click()
+                            time.sleep(10)
+                            current_page = driver.find_element_by_xpath(
+                                '//span[@class="yui-pg-current-page yui-pg-page"]').text
+                            if current_page != str(page_num):
+                                raise
+                            else:
+                                break
+                        except:
+                            print(f"retrying...#{retry} in {iv} sec(s)")
+                            if retry == rt:
+                                print("Failed")
+                                sys.exit(1)
+                            else:
+                                time.sleep(iv)
+                                retry += 1
+
+            # writing metadata to JSON file
+            print("Writing metadata...")
+            with open(GISAID_JASON, 'w') as outfile:
+                json.dump(metadata, outfile)
+
+        # wait for download to complete
+        if not os.path.isfile(GISAID_FASTA) or not os.path.isfile(GISAID_TABLE):
+            time.sleep(30)
+        if not os.path.isfile(GISAID_FASTA) or not os.path.isfile(GISAID_TABLE):
+            time.sleep(30)
+        if os.path.isfile(GISAID_FASTA) and os.path.isfile(GISAID_TABLE):
+            while (os.stat(GISAID_FASTA).st_size == 0):
+                time.sleep(5)
+            while (os.stat(GISAID_TABLE).st_size == 0):
+                time.sleep(5)
 
     # close driver
     driver.quit()
